92fdd850e4b385f4639d5ee05f241a579b4e5f9f
Md. Tonoy Akando <tonoy.sust@gmail.com>
2022-12-31 16:49:58 +0600

    learning spring framework

---
+++ src/test/java/me/tonoy/learnspringboot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tonoy.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
